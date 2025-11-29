27af6bd73c0cb6855077a62be571ff6a1d617281
XJY <jiayi_xie_xjtu@outlook.com>
2025-11-29 12:11:24 +0800

    modify the odomquadruped topic to nav msgs format

--- src/transform_odom.cpp
+++ src/transform_odom.cpp
@@ -3,11 +3,13 @@
 #include <nav_msgs/Odometry.h>
 #include <Eigen/Eigen>
 #include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Twist.h>
 #include <cmath>  
  
 Eigen::Vector3d pos;
 Eigen::Quaterniond quaternion;
- 
+geometry_msgs::Twist saved_twist;
+
 void odom_src_callback(const nav_msgs::Odometry::ConstPtr &msg)
 {
     if(msg->header.frame_id == "world")
@@ -17,7 +19,8 @@ void odom_src_callback(const nav_msgs::Odometry::ConstPtr &msg)
         quaternion = Eigen::Quaterniond(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
         Eigen::Quaterniond rot_z(Eigen::AngleAxisd(-M_PI/2, Eigen::Vector3d::UnitZ()));
         quaternion = rot_z * quaternion;
-
+        // save incoming velocity so we can forward it in odom_tar
+        saved_twist = msg->twist.twist;
         static tf::TransformBroadcaster br;
         tf::Transform transform;
         transform.setOrigin(tf::Vector3(pos(0), pos(1), pos(2)));
@@ -34,7 +37,7 @@ int main(int argc, char **argv)
  
     ros::Subscriber slam_sub = nh.subscribe<nav_msgs::Odometry>("odom_src", 100, odom_src_callback);
  
-    ros::Publisher odom_tar_pub = nh.advertise<geometry_msgs::PoseStamped>("odom_tar", 10);
+    ros::Publisher odom_tar_pub = nh.advertise<nav_msgs::Odometry>("odom_tar", 10);
  
  
     // the setpoint publishing rate MUST be faster than 2Hz
@@ -43,17 +46,20 @@ int main(int argc, char **argv)
     ros::Time last_request = ros::Time::now();
  
     while(ros::ok()){
-        geometry_msgs::PoseStamped odom_tar;
- 
-        odom_tar.pose.position.x = pos[0];
-        odom_tar.pose.position.y = pos[1];
-        odom_tar.pose.position.z = pos[2];
- 
-        odom_tar.pose.orientation.x = quaternion.x();
-        odom_tar.pose.orientation.y = quaternion.y();
-        odom_tar.pose.orientation.z = quaternion.z();
-        odom_tar.pose.orientation.w = quaternion.w();
- 
+        nav_msgs::Odometry odom_tar;
+
+        odom_tar.pose.pose.position.x = pos[0];
+        odom_tar.pose.pose.position.y = pos[1];
+        odom_tar.pose.pose.position.z = pos[2];
+
+        odom_tar.pose.pose.orientation.x = quaternion.x();
+        odom_tar.pose.pose.orientation.y = quaternion.y();
+        odom_tar.pose.pose.orientation.z = quaternion.z();
+        odom_tar.pose.pose.orientation.w = quaternion.w();
+
+        // copy velocity from the latest received odom_src
+        odom_tar.twist.twist = saved_twist;
+
         odom_tar.header.stamp = ros::Time::now();
         odom_tar.header.frame_id = "world";
         odom_tar_pub.publish(odom_tar);
